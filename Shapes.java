9e995061b306e6be60841e3209ca0e2a2bf0302c
sunil58 <41838700+sunil58@users.noreply.github.com>
2018-09-06 20:31:21 +0530

    Add files via upload

---
+++ Shapes.java
@@ -0,0 +1,45 @@
+class Shapes 
+{
+		public void rotate()
+	    {
+			System.out.println("Rotate ClockWise...");
+		}
+	
+}
+
+class Circle extends Shapes
+{
+	public static void main(String[] args) 
+	{
+		Circle rv1=new Circle();
+		rv1.rotate();
+	}
+}
+
+class Rect extends Shapes
+{
+	public void rotate()
+	{
+		System.out.println("Rotate Anti-ClockWise...");
+	}
+	public static void main(String[] args) 
+	{
+		Rect rv1=new Rect();
+		rv1.rotate();
+	}
+}
+
+class Tri extends Shapes
+{
+	public void rotate()
+	{   
+		super.rotate();
+		System.out.println("Rotate Anti-ClockWise...");
+	}
+
+	public static void main(String[] args) 
+	{
+		Tri rv1=new Tri();
+		rv1.rotate();
+	}
+}
